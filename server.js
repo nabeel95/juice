de2e3358c8064236869f7ecbe08f002e93602e99
Nabeel <nabeel@thoughtworks.com>
2016-06-15 20:04:52 +0530

    added details in server file

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 var controller = require('./public/js/controller');
 var server = http.createServer(controller);
 server.listen(3000);
-console.log("server started");
+console.log("server started at port 3000");
